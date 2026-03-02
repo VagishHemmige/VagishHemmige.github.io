54b99ad54e7b2cbeb52ff4bdea29c84ba1c4ff3e
Vagish Hemmige <vahemmig@montefiore.org>
2026-03-01 21:54:15 -0500

    Update .docx of CV

--- R/cv.R
+++ R/cv.R
@@ -1,1 +1,11 @@
 #This script creates a CV from a template
+
+library(officer)
+library(RefManageR)
+library(stringr)
+
+doc <- read_docx("cv/Hemmige Einstein CV template.docx")
+
+
+filename<-paste0("cv/Hemmige Einstein CV ", Sys.Date(), ".docx")
+print(doc, target = filename)
